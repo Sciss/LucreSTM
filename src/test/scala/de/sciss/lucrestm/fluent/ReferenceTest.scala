497d5fe91feec0a6e7ed60cc9e40d4369ff657ec
Sciss <contact@sciss.de>
2011-12-25 23:24:02 +0100

    step

--- src/test/scala/de/sciss/lucrestm/fluent/ReferenceTest.scala
+++ src/test/scala/de/sciss/lucrestm/fluent/ReferenceTest.scala
@@ -12,28 +12,82 @@ object ReferenceTest extends App {
       implicit def serializer[ S <: Sys[ S ]] : TxnSerializer[ S#Tx, S#Acc, Sink[ S ]] = new Ser[ S ]
 
       private final class Ser[ S <: Sys[ S ]] extends TxnSerializer[ S#Tx, S#Acc, Sink[ S ]] {
-         def write( v: Sink[ S ], out: DataOutput ) {
-            sys.error( "TODO" )
-         }
+         def write( v: Sink[ S ], out: DataOutput ) { v.write( out )}
 
          def read( in: DataInput, access: S#Acc)( implicit tx: S#Tx ) : Sink[ S ] = {
+            new SinkRead[ S ]( in, tx, access )
+         }
+      }
+
+      def apply[ S <: Sys[ S ]]()( implicit tx: S#Tx ) : Sink[ S ] = new SinkNew[ S ]( tx )
+
+      private final class SinkNew[ S <: Sys[ S ]]( tx0: S#Tx ) extends Sink[ S ] {
+         val id = tx0.newID()
+         protected val sinks = tx0.newVar[ IIdxSeq[ Sink[ S ]]]( id, IIdxSeq.empty )
+      }
+
+      private final class SinkRead[ S <: Sys[ S ]]( in: DataInput, tx0: S#Tx, acc: S#Acc ) extends Sink[ S ] {
+         val id = tx0.readID( in, acc )
+         protected val sinks = tx0.readVar[ IIdxSeq[ Sink[ S ]]]( id, in )
+      }
+   }
+   sealed trait Sink[ S <: Sys[ S ]] extends Mutable[ S ] {
+//      def addSink( sink: Sink[ S ])( implicit tx: S#Tx ) : Unit
+//      def removeSink( sink: Sink[ S ])( implicit tx: S#Tx ) : Unit
+      final def propagate()( implicit tx: S#Tx, map: LiveMap[ S ]) {
+         map.propagate( this )
+         sinks.get.foreach( _.propagate() )
+      }
+
+      final protected def writeData( out: DataOutput ) {
+         sinks.write( out )
+      }
+
+      final protected def disposeData()( implicit tx: S#Tx ) {
+         sinks.dispose()
+      }
+
+      protected def sinks: S#Var[ IIdxSeq[ Sink[ S ]]]
+
+//      final def addSink( sink: Sink[ S ])( implicit tx: S#Tx ) {
+//         val xs = sinks.get
+//         sinks.set( xs :+ sink )
+//         if( xs.isEmpty /* && reactions.get.isEmpty */) {
+//            deploy()
+//         }
+//      }
+//
+//      final def removeSink( sink: Sink[ S ])( implicit tx: S#Tx ) {
+//         val xs = sinks.get
+//         val i = xs.indexOf( sink )
+//         if( i >= 0 ) {
+//            val xs1 = xs.patch( i, IIdxSeq.empty, 1 )
+//            sinks.set( xs1 )
+//            if( xs1.isEmpty /* && reactions.get.isEmpty */) undeploy()
+//         }
+//      }
+   }
+
+   object LiveMap {
+      def apply[ S <: Sys[ S ]]() : LiveMap[ S ] = new Impl[ S ]
+
+      private final class Impl[ S <: Sys[ S ]] extends LiveMap[ S ] {
+         def propagate( sink: Sink[ S ])( implicit tx: S#Tx ) {
             sys.error( "TODO" )
          }
       }
    }
-   trait Sink[ S <: Sys[ S ]] {
-      def propagate()( implicit tx: S#Tx, map: LiveMap[ S ]) : Unit
-   }
-
-   trait LiveMap[ S <: Sys[ S ]] {
-      def addReaction[    A ]( event: Event[ S, A ], fun: A => Unit )( implicit tx: S#Tx ) : Unit
-      def removeReaction[ A ]( event: Event[ S, A ], fun: A => Unit )( implicit tx: S#Tx ) : Unit
-      def getReactions[   A ]( event: Event[ S, A ])( implicit tx: S#Tx ) : Traversable[ A => Unit ]
+   sealed trait LiveMap[ S <: Sys[ S ]] {
+//      def addReaction[    A ]( event: Event[ S, A ], fun: A => Unit )( implicit tx: S#Tx ) : Unit
+//      def removeReaction[ A ]( event: Event[ S, A ], fun: A => Unit )( implicit tx: S#Tx ) : Unit
+//      def getReactions[   A ]( event: Event[ S, A ])( implicit tx: S#Tx ) : Traversable[ A => Unit ]
+      def propagate( sink: Sink[ S ])( implicit tx: S#Tx ) : Unit
    }
 
    trait Event[ S <: Sys[ S ], A ] {
-      def addSink( sink: Sink[ S ])( implicit tx: S#Tx ) : Unit
-      def removeSink( sink: Sink[ S ])( implicit tx: S#Tx ) : Unit
+      protected def sink: Sink[ S ]
+//      def addSink( sink: Sink[ S ])( implicit tx: S#Tx ) : Unit
+//      def removeSink( sink: Sink[ S ])( implicit tx: S#Tx ) : Unit
       protected def deploy()( implicit tx: S#Tx ) : Unit
       protected def undeploy()( implicit tx: S#Tx ) : Unit
       protected def propagate( v: A )( implicit tx: S#Tx, map: LiveMap[ S ]) : Unit
@@ -92,10 +146,10 @@ object ReferenceTest extends App {
 //         }
       }
 
-      protected final def propagate( v: A )( implicit tx: S#Tx, map: LiveMap[ S ]) {
-         map.getReactions( this ).foreach( _.apply( v ))
-         sinks.get.foreach( _.propagate() )
-      }
+//      protected final def propagate( v: A )( implicit tx: S#Tx, map: LiveMap[ S ]) {
+//         map.getReactions( this ).foreach( _.apply( v ))
+//         sinks.get.foreach( _.propagate() )
+//      }
    }
 
    trait Expr[ A ] {
