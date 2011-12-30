0cfc1f17c05216e0d3f6df555d63e0d3bff1e922
Sciss <contact@sciss.de>
2011-12-30 01:11:40 +0100

    compiles but not sure it's correct with those crappy castings

--- src/test/scala/de/sciss/lucrestm/fluent/Reaction2.scala
+++ src/test/scala/de/sciss/lucrestm/fluent/Reaction2.scala
@@ -3,7 +3,7 @@ package de.sciss.lucrestm.fluent
 import collection.immutable.{IndexedSeq => IIdxSeq}
 
 object Reaction2 extends App {
-   type Reaction = () => Unit
+   type Reaction = () => () => Unit
    type Reactions = IIdxSeq[ Reaction ]
    type Reactors  = IIdxSeq[ Reactor ]
 
@@ -15,13 +15,19 @@ object Reaction2 extends App {
    trait Observable extends ObservableStub {
       def addSink( r: Reactor ) : Unit
       def removeSink( r: Reactor ) : Unit
+      def addObserver( id: Int ) : Unit
+      def removeObserver( in: Int ) : Unit
    }
 
    trait Reactor {
       def propagate( id: AnyRef, reactions: Reactions ) : Reactions
    }
 
-   trait Observer[ A ] {
+   trait Removable {
+      def remove() : Unit
+   }
+
+   trait Observer[ A ] extends Removable {
       def apply( value: A ) : Unit
       def remove() : Unit
    }
@@ -30,25 +36,38 @@ object Reaction2 extends App {
       def read( b: BranchStub ) : A
    }
 
-   final case class Observation[ A ]( reader: Reader[ A ], fun: A => Unit )
+   final case class Observation[ A, Repr <: Eval[ A ]]( reader: Reader[ Repr ], fun: (Repr, A) => Unit )
 
    object Txn {
       private var cnt = 0
-      private var map = Map.empty[ Int, Observation[ _ ]]
+      private var map = Map.empty[ Int, Observation[ _, _ ]]
 
-      def mapSource[ A ]( value: A, observerKeys: IIdxSeq[ Int ]) : Reactions = {
+      def mapSource[ A, Repr <: Eval[ A ]]( value: Repr, observerKeys: IIdxSeq[ Int ]) : Reactions = {
          observerKeys.flatMap { key =>
             map.get( key ).map { observation =>
-               () => observation.asInstanceOf[ Observation[ A ]].fun( value )
+               () => {
+                  val eval = value.eval
+                  () => observation.asInstanceOf[ Observation[ A, Repr ]].fun( value, eval )
+               }
             }
          }
       }
 
-      def addObservation[ A ]( obs: Observation[ A ]) : Int = {
+      def addObservation[ A, Repr <: Eval[ A ]]( observable: Observable,
+                                                 reader: Reader[ Repr ], fun: (Repr, A) => Unit ) : Removable = {
          val key = cnt
          cnt += 1
+
+         val obs = Observation[ A, Repr ]( reader, fun )
          map += key -> obs
-         key
+
+         val res = new Removable {
+            def remove() {
+               Txn.removeObservation( key )
+            }
+         }
+         observable.addObserver( key )
+         res
       }
 
       def removeObservation( key: Int ) {
@@ -59,9 +78,15 @@ object Reaction2 extends App {
          val observations = observerKeys.flatMap( map.get( _ ))
          observations.headOption match {
             case Some( obs ) =>
-               val full = obs.reader.read( b ).asInstanceOf[ AnyRef ]
-               observations.map( obs2 => () => obs2.fun.asInstanceOf[ AnyRef => Unit ].apply( full ))
-            case None => IIdxSeq.empty
+               val full = obs.reader.read( b ).asInstanceOf[ Eval[ AnyRef ]]
+               val funs = observations.map( _.fun ).asInstanceOf[ IIdxSeq[ (Eval[ AnyRef ], AnyRef) => Unit ]]
+               val react: Reaction = () => {
+                  val eval = full.eval
+                  () => funs.foreach( _.apply( full, eval ))
+               }
+               reactions :+ react
+
+            case None => reactions
          }
       }
    }
@@ -76,33 +101,35 @@ object Reaction2 extends App {
       }
    }
 
-   trait Branch[ Repr ] extends BranchLike with Observable {
+   trait Eval[ A ] { def eval: A }
+
+   trait Branch[ A, Repr <: Eval[ A ]] extends BranchLike with Observable {
+      me: Repr =>
+
       def reader: Reader[ Repr ]
-      def observe( fun: Repr => Unit ) : Observer[ Repr ] = {
-         val key = Txn.addObservation( Observation( reader, fun ))
-         new Observer[ Repr ] {
-            def apply( value: Repr ) { fun( value )}
-            def remove() {
-               Txn.removeObservation( key )
-            }
-         }
+
+      def observe( fun: (Repr, A) => Unit ) : Removable = {
+         Txn.addObservation( this, reader, fun )
       }
    }
 
    object LongExpr {
       val reader: Reader[ LongExpr ] = sys.error( "-" )
    }
-   trait LongExpr extends Branch[ LongExpr ] {
+   trait LongExpr extends Branch[ Long, LongExpr ] with Eval[ Long ] {
       def reader = LongExpr.reader
    }
 
-   trait Source extends Observable {
+   trait Source[ A, Repr <: Eval[ A ]] extends Observable {
+      me: Repr =>
+
       def bang() {
          val id = new AnyRef
 //         sinks.foreach( _.propagate( id ))
-         val reactions  = Txn.mapSource( this, observerKeys )
+         val reactions  = Txn.mapSource[ A, Repr ]( this: Repr, observerKeys )
          val reactions2 = sinks.foldLeft( reactions ) { case (reactionsIter, sink) => sink.propagate( id, reactionsIter )}
-         reactions2.foreach( _.apply() )
+         val evaluated = reactions2.map( _.apply() )
+         evaluated.foreach( _.apply() )
       }
    }
 }
