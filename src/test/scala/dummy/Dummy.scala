cc60b9e1a4783ebbfd7cfdd98b6d6bd4de36183c
Sciss <contact@sciss.de>
2012-09-26 00:19:09 +0200

    solution posted to SO. still horrible

--- src/test/scala/dummy/Dummy.scala
+++ src/test/scala/dummy/Dummy.scala
@@ -34,108 +34,96 @@ package dummy
 
 trait VarLike[ -Tx, A ] { def update( value: A )( implicit tx: Tx ) : Unit }
 
-trait S2[ S <: S2[ S ]] {
-//   _: S =>
-
-   type Tx <: T2[ S /*, Tx */]
-
+trait Sys[ S <: Sys[ S ]] {
+   type Tx       <: Txn[ S ]
    type Var[ A ] <: VarLike[ S#Tx, A ]
    type ID
+   type Peer     <: Sys[ Peer ]
 
-   type Peer <: S2[ Peer ]
-
-//   def confirm( tx: S#Tx ) : S#Tx = tx
-//   def convert( tx: S#Tx ) : S#Peer#Tx
-
+   // this indirection works, but is horrible
    def proxyUpdate[ A ]( v: S#Peer#Var[ A ], value: A )( implicit tx: S#Tx ) : Unit
    def proxyNewID()( implicit tx: S#Tx ) : S#Peer#ID
    def proxyNewVar[ A ]( id: S#Peer#ID, init: A )( implicit tx: S#Tx ) : S#Peer#Var[ A ]
 
-   def proxy( implicit tx: S#Tx ) : S#Peer#Tx
+   def proxy( implicit tx: S#Tx ) : S#Peer#Tx  // couldn't make use of this
 }
 
-trait T2[ S <: S2[ S ] /*, T <: T2[ S, T ]*/] {
-//   _: T =>
-
-   type S1 = S
-
+trait Txn[ S <: Sys[ S ]] {
    def newID() : S#ID
    def newVar[ A ]( id: S#ID, init: A ) : S#Var[ A ]
-
    def system: S
-   def peer: S#Peer#Tx
+   def peer: S#Peer#Tx // only useful for the system instance with types fixed
 }
 
-class InMemTx( val system: InMem ) extends T2[ InMem /*, InMemTx */ ] {
+// let's make sure we can implement actual systems
+class InMemTx( val system: InMem ) extends Txn[ InMem ] {
    def newID() {}
    def newVar[ A ]( id: InMem#ID, init: A ) : InMem#Var[ A ] = new VarLike[ InMemTx, A ] {
       def update( v: A )( implicit tx: InMemTx ) {}
    }
    def peer = this
 }
-class InMem extends S2[ InMem ] {
-   type Tx = InMemTx
+class InMem extends Sys[ InMem ] {
+   type Tx       = InMemTx
    type Var[ A ] = VarLike[ Tx, A ]
-   type ID = Unit
-
-   type Peer = InMem
-
-//   def convert( tx: InMem#Tx ) = tx
+   type ID       = Unit
+   type Peer     = InMem  // reflect back to ourself
 
    def proxyUpdate[ A ]( v: Var[ A ], value: A )( implicit tx: Tx ) {
       v.update( value )( tx )
    }
-
    def proxyNewID()( implicit tx: Tx ) {}
    def proxyNewVar[ A ]( id: ID, init: A )( implicit tx: Tx ) : Var[ A ] = tx.newVar( id, init )
 
    def proxy( implicit tx: Tx ) : Tx = tx
 }
 
-class DurableTx( val system: Durable, val peer: InMem#Tx ) extends T2[ Durable /*, DurableTx */ ] {
+class DurableTx( val system: Durable, val peer: InMem#Tx ) extends Txn[ Durable ] {
    def newID() = 33
    def newVar[ A ]( id: Durable#ID, init: A ) : Durable#Var[ A ] = new VarLike[ DurableTx, A ] {
       def update( v: A )( implicit tx: DurableTx ) {}
    }
 }
-class Durable extends S2[ Durable ] {
+class Durable extends Sys[ Durable ] {
    type Tx = DurableTx
    type Var[ A ] = VarLike[ Tx, A ]
    type ID = Int
-
    type Peer = InMem
-
-//   def convert( tx: InMem#Tx ) = tx
 
    def proxyUpdate[ A ]( v: InMem#Var[ A ], value: A )( implicit tx: Tx ) {
       v.update( value )( tx.peer )
    }
-
    def proxyNewID()( implicit tx: Tx ) {}
    def proxyNewVar[ A ]( id: InMem#ID, init: A )( implicit tx: Tx ) : InMem#Var[ A ] = tx.peer.newVar( id, init )
 
    def proxy( implicit tx: Tx ) : InMem#Tx = tx.peer
 }
 
-trait WithT2[ S <: S2[ S ]] {
-   def v1 : S#Var[ Int ]
+// let's make sure we can use the system as intended
+trait TestTrait[ S <: Sys[ S ]] {
+   def v1 : S#Peer#Var[ Int ]
    def v2 : S#Peer#Var[ Int ]
 
-   def test( implicit tx: S#Tx ) {
-//      tx.system.confirm( tx )
-      v1.update( 1 )( tx )
+   def test1( implicit tx: S#Tx ) {
+      tx.system.proxyUpdate( v1, 1 )
+   }
 
-//      val p = tx.system.convert( tx )
+   def test2( implicit tx: S#Tx ) {
+// no way to get around the direct proxy calls...
+//      val p = tx.peer
+//      val p = tx.system.proxy( tx )
 //      v2.update( 1 )( p )
-
-      tx.system.proxyUpdate( v2, 1 )
    }
 }
 
-class WithT2C[ S <: S2[ S ]]( implicit tx: S#Tx ) extends WithT2[ S ] {
-   val v1 = tx.newVar( tx.newID(), 0 )
-   val v2 = tx.system.proxyNewVar( tx.system.proxyNewID(), 0 )
-
-   test
+// see if we can actually create variables
+class TestImpl[ S <: Sys[ S ]]( implicit tx: S#Tx ) extends TestTrait[ S ] {
+   val v1 = tx.system.proxyNewVar( tx.system.proxyNewID(), 0 )
+   val v2 = {
+// no way to get around the direct proxy calls...
+//      val p = tx.peer
+//      val p = tx.system.proxy( tx )
+//      p.newVar( p.newID(), 0 )
+      sys.error( "???" )
+   }
 }
-
